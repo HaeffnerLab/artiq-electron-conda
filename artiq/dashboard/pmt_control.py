91220a257ad50e2d99f061146c4a3cb263d94bc6
Joseph Broz <jbroz@berkeley.edu>
2019-03-14 19:09:12 -0700

    finish up with pmt control start on linetrigger

--- artiq/dashboard/pmt_control.py
+++ artiq/dashboard/pmt_control.py
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 
 class PMTControlDock(QtWidgets.QDockWidget):
     def __init__(self, main_window, exp_manager):
-        QtWidgets.QDockWidget.__init__(self, "PMT Control")
+        QtWidgets.QDockWidget.__init__(self, "PMT / Linetrigger Control")
         self.setObjectName("pmt_control")
         self.setFeatures(QtWidgets.QDockWidget.DockWidgetMovable |
                          QtWidgets.QDockWidget.DockWidgetFloatable)
@@ -22,20 +22,28 @@ class PMTControlDock(QtWidgets.QDockWidget):
         self.dset_ctl = Client("::1", 3251, "master_dataset_db")
         self.scheduler = Client("::1", 3251, "master_schedule")
         self.rid = None
-        self.expid = {"arguments": {},
-                      "class_name": "pmtcollect",
-                      "file": "run_continuously/run_pmt.py",
-                      "log_level": 30,
-                      "repo_rev": "N/A"}
+        self.pulsed = False
+        self.expid_continuous = {"arguments": {},
+                                "class_name": "pmt_collect_continuously",
+                                "file": "run_continuously/run_pmt_continuously.py",
+                                "log_level": 30,
+                                "repo_rev": "N/A"}
 
+        self.expid_pulsed = {"arguments": {},
+                             "class_name": "pmt_collect_pulsed",
+                             "file": "run_continuously/run_pmt_pulsed.py",
+                             "log_level": 30,
+                             "repo_rev": "N/A"}
+
+        frame = QtWidgets.QFrame()
+        frame.setFrameShape(QtWidgets.QFrame.StyledPanel)
+        frame.setFrameShadow(QtWidgets.QFrame.Raised)
         layout = QtWidgets.QGridLayout()
-        top_widget = QtWidgets.QWidget()
-        vLayout = QtWidgets.QVBoxLayout(top_widget)
+        vLayout = QtWidgets.QVBoxLayout(frame)
         vLayout.addLayout(layout)
         vLayout.addStretch(1)
 
-        # top_widget.setLayout(layout)
-        self.setWidget(top_widget)
+        self.setWidget(frame)
 
         self.shortcut_widgets = dict()
 
@@ -60,19 +68,30 @@ class PMTControlDock(QtWidgets.QDockWidget):
         self.p.set_parameter(["PmtReadout", "duration", U(100, "ms")])
         validator = QtGui.QDoubleValidator()
         self.duration.setValidator(validator)
-        self.duration.returnPressed.connect(self.check_state)
+        self.duration.returnPressed.connect(self.duration_changed)
         self.duration.setStyleSheet("QLineEdit { background-color:  #c4df9b}" )
 
-        self.modeLabel = QtWidgets.QLabel("mode: ")
+        self.modeLabel = QtWidgets.QLabel("Mode: ")
         self.setMode = QtWidgets.QComboBox()
         self.setMode.addItem("continuous")
-        self.p.set_parameter(["PmtReadout", "pulsed", False])
         self.setMode.addItem("pulsed")
         self.setMode.currentIndexChanged.connect(self.set_mode)
 
+        self.linetriggerButton = QtWidgets.QPushButton("Off")
+        self.linetriggerButton.clicked[bool].connect(self.toggle_linetrigger)
+        self.linetriggerLineEdit = QtWidgets.QLineEdit("0")
+        self.linetriggerLineEdit.returnPressed.connect(self.linetrigger_duration_changed)
+
+        boldFont = QtGui.QFont()
+        boldFont.setBold(True)
+        self.pmtLabel = QtWidgets.QLabel("PMT")
+        self.pmtLabel.setFont(boldFont)
+        self.linetriggerLabel = QtWidgets.QLabel("LINETRIGGER")
+        self.linetriggerLabel.setFont(boldFont)
+
         layout.addWidget(QtWidgets.QLabel(""), 0, 0)
         layout.addWidget(QtWidgets.QLabel(""), 1, 0)
-        layout.addWidget(QtWidgets.QLabel(""), 2, 0)
+        layout.addWidget(self.pmtLabel, 2, 1)
         layout.addWidget(self.onButton, 3, 0)
         layout.addWidget(self.countDisplay, 3, 1)
         layout.addWidget(self.unitsLabel, 3, 2)
@@ -80,22 +99,38 @@ class PMTControlDock(QtWidgets.QDockWidget):
         layout.addWidget(self.duration, 4, 1, 1, 2)
         layout.addWidget(self.modeLabel, 5, 0)
         layout.addWidget(self.setMode, 5, 1, 1, 2)
+        layout.addWidget(QtWidgets.QLabel(""), 6, 0)
+        layout.addWidget(QtWidgets.QLabel(""), 7, 0)
+        layout.addWidget(QtWidgets.QLabel(""), 8, 0)
+        layout.addWidget(QtWidgets.QLabel(""), 9, 0)
+        layout.addWidget(self.linetriggerLabel, 10, 1)
+        layout.addWidget(self.linetriggerButton, 11, 0, 1, 3)
+        layout.addWidget(QtWidgets.QLabel("Offset duration (us): "), 12, 0)
+        layout.addWidget(self.linetriggerLineEdit, 12, 1, 1, 2)
 
-    def set_state(self):
-        if self.onButton.isChecked():
+    def set_state(self, override=False):
+        if override:
+            flag = True
+        else:
+            flag = self.onButton.isChecked()
+
+        if flag:
             if self.rid is None:
-                self.rid = self.scheduler.submit("main", self.expid, 0)
+                if self.setMode.currentText() == "continuous":
+                    self.rid = self.scheduler.submit("main", self.expid_continuous, 0)
+                else:
+                    self.rid = self.scheduler.submit("main", self.expid_pulsed, 0)
             self.onButton.setText("Off")
 
         else:
             if self.rid is None:
                 return  # Shouldn't happen
             else:
-                self.scheduler.request_termination(self.rid)
+                self.scheduler.delete(self.rid)
                 self.rid = None
             self.onButton.setText("On")
 
-    def check_state(self, *args, **kwargs):
+    def duration_changed(self, *args, **kwargs):
         # connect to parametervault here
         sender = self.sender()
         validator = sender.validator()
@@ -111,16 +146,35 @@ class PMTControlDock(QtWidgets.QDockWidget):
             min = 1e-3 # 1 us
             raw_duration = float(sender.text())
             duration = raw_duration if raw_duration >= min else min
-            self.p.set_parameter(["PmtReadout", "duration", U(float(sender.text()), "ms")])
+            self.p.set_parameter(["PmtReadout", "duration", U(duration, "ms")])
+            if self.rid is None:
+                return
+            else:
+                self.scheduler.delete(self.rid)
+                self.rid = None
+                self.set_state(True)
         except ValueError:
             # Shouldn't happen
             pass
 
     def set_mode(self):
-        if str(self.setMode.currentText()) == "continuous":
-            self.p.set_parameter(["PmtReadout", "pulsed", False])
+        txt = str(self.setMode.currentText())
+        if self.rid is None:
+            self.pulsed = txt == "pulsed"  
+        elif  txt == "continuous":
+            if not self.pulsed:
+                return
+            else:
+                self.pulsed = False
+                self.scheduler.delete(self.rid)
+                self.rid = self.scheduler.submit("main", self.expid_continuous, 0)
         else:
-            self.p.set_parameter(["PmtReadout", "pulsed", True])
+            if self.pulsed:
+                return
+            else:
+                self.pulsed = True
+                self.scheduler.delete(self.rid)
+                self.rid = self.scheduler.submit("main", self.expid_pulsed, 0)
 
     def updateLCD(self):
         if not self.onButton.isChecked():
@@ -141,3 +195,9 @@ class PMTControlDock(QtWidgets.QDockWidget):
         except IndexError:
             # timer too fast
             pass         
+
+    def toggle_linetrigger(self):
+        pass
+
+    def linetrigger_duration_changed(self):
+        pass
