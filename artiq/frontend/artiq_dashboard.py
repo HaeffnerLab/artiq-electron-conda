81d017d8c4f3e63bebdacaaeadc190ac7af831a2
Joseph Broz <jbroz@berkeley.edu>
2019-03-15 18:02:28 -0700

    add save for pmtcontrol

--- artiq/frontend/artiq_dashboard.py
+++ artiq/frontend/artiq_dashboard.py
@@ -86,7 +86,7 @@ class MdiArea(QtWidgets.QMdiArea):
         painter = QtGui.QPainter(self.viewport())
         x = (self.width() - self.pixmap.width())//2
         y = (self.height() - self.pixmap.height())//2
-        painter.setOpacity(1)
+        painter.setOpacity(0.5)
         painter.drawPixmap(x, y, self.pixmap)
 
 
@@ -151,11 +151,8 @@ def main():
         broadcast_clients[target] = client
 
     # initialize main window
-    tabs = QtWidgets.QTabWidget()
-    main_main_window = MainWindow(args.server if server_name is None else server_name)
-    main_main_window.setCentralWidget(tabs)
     main_window = MainWindow(args.server if server_name is None else server_name)
-    smgr.register(main_main_window)
+    smgr.register(main_window)
     mdi_area = MdiArea()
     mdi_area.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAsNeeded)
     mdi_area.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAsNeeded)
@@ -170,6 +167,8 @@ def main():
     smgr.register(expmgr)
     d_shortcuts = shortcuts.ShortcutsDock(main_window, expmgr)
     smgr.register(d_shortcuts)
+    d_pmt = pmt_control.PMTControlDock(main_window, expmgr)
+    smgr.register(d_pmt)
     d_explorer = explorer.ExplorerDock(expmgr, d_shortcuts,
                                        sub_clients["explist"],
                                        sub_clients["explist_status"],
@@ -237,16 +236,9 @@ def main():
     logging.info("ARTIQ dashboard %s connected to %s",
                  artiq_version, server_description)
 
-    histograms_tab = QtWidgets.QTabWidget()
-    drift_tracker_tab = QtWidgets.QWidget()
-
     # run
-    tabs.addTab(main_window, "Control")
-    tabs.addTab(histograms_tab, "Readout")
-    tabs.addTab(drift_tracker_tab, "Drift Tracker")
-    main_main_window.show()
-    loop.run_until_complete(main_main_window.exit_request.wait())
-
+    main_window.show()
+    loop.run_until_complete(main_window.exit_request.wait())
 
 if __name__ == "__main__":
     main()
