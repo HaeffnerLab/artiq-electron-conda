67a7c0179cf3329481a7cbe476c166264bdda130
Ryan Shaffer <rmshaffer@gmail.com>
2019-04-28 15:22:50 -0700

    PMT plot window should start a new curve whenever dataset is reset

--- artiq/applets/plot_pmt.py
+++ artiq/applets/plot_pmt.py
@@ -13,6 +13,8 @@ class PMTPlot(pyqtgraph.PlotWidget):
         pyqtgraph.PlotWidget.__init__(self)
         self.args = args
         self.curves = {}
+        self.current_curve_x_start = {}
+        self.current_curve_point_count = {}
         self.showGrid(x=True, y=True, alpha=0.75)
         self.setYRange(0, 1000)
 
@@ -47,48 +49,47 @@ class PMTPlot(pyqtgraph.PlotWidget):
                    "with_866_off": pyqtgraph.mkBrush((0,0,255,75)),
                    "diff_counts":  pyqtgraph.mkBrush((0,255,0,75))}
 
-        data_points_per_curve = 1000
-
         # we want to plot specifically in this order: with_866_on, diff_counts, with_866_off
         # so that the brushes look nice visually
         for curve_name in ["with_866_on", "diff_counts", "with_866_off"]:
             if not curve_name in raw_data.keys():
                 continue
 
-            data_to_plot = raw_data[curve_name]            
+            data_to_plot = raw_data[curve_name]
             num_points = len(data_to_plot)
-            if num_points == 0:
-                continue
-
-            x = np.arange(num_points)
-            num_curves_needed = (num_points // data_points_per_curve) + 1
 
             if not curve_name in self.curves.keys():
                 self.curves[curve_name] = []
 
-            num_curves_now = len(self.curves[curve_name])
-            if num_curves_needed < num_curves_now:
-                # if we have more curves than necessary, we must have restarted the dataset,
-                # so remove all curves and start over
-                for curve in self.curves[curve_name]:
-                    self.removeItem(curve)
-                self.curves[curve_name] = []
-                num_curves_now = 0
+            if not curve_name in self.current_curve_point_count.keys():
+                self.current_curve_point_count[curve_name] = 0
+
+            if not curve_name in self.current_curve_x_start.keys():
+                self.current_curve_x_start[curve_name] = 0
+
+            if num_points < self.current_curve_point_count[curve_name]:
+                # if this dataset has fewer points than the current curve, the dataset
+                # must have been restarted, so we will simply begin a new curve.
+                # update the x_start value for the new curve
+                self.current_curve_x_start[curve_name] += self.current_curve_point_count[curve_name]
             else:
-                # otherwise, let's remove the latest one (in case it needs to be updated)
+                # we have more points, so the current curve needs to be updated.
+                # remove the current curve so that we will recreate it.
+                num_curves_now = len(self.curves[curve_name])
                 if num_curves_now > 0:
                     latest_curve = self.curves[curve_name][-1]
                     self.removeItem(latest_curve)
                     self.curves[curve_name].pop(-1)
                     num_curves_now -= 1
 
-            # now let's plot all the curves that haven't already been plotted
-            for curve_index in range(num_curves_now, num_curves_needed):
-                x_start = curve_index * data_points_per_curve
-                x_end = min(num_points, x_start + data_points_per_curve + 1)
-                self.curves[curve_name].append(self.plot(
-                    x[x_start:x_end], data_to_plot[x_start:x_end],
-                    pen=pens[curve_name], brush=brushes[curve_name], fillLevel=0))
+            self.current_curve_point_count[curve_name] = num_points
+
+            x_start = self.current_curve_x_start[curve_name]
+            x_end = x_start + num_points
+            x = np.arange(x_start, x_end)
+            self.curves[curve_name].append(self.plot(
+                x, data_to_plot,
+                pen=pens[curve_name], brush=brushes[curve_name], fillLevel=0))
 
         if self.autoscroll:
             (xmin_cur, xmax_cur), _ = self.viewRange()
