f291f51beadd07d265d9760590cc1b008d0aa0cf
Joseph Broz <jbroz@berkeley.edu>
2019-04-30 18:20:17 -0700

    some work on ndim scan

--- artiq/applets/rcg/RealComplicatedGrapherConfig.py
+++ artiq/applets/rcg/RealComplicatedGrapherConfig.py
@@ -11,7 +11,7 @@ port = 3286
 data_dir = "/home/lattice/data"
 
 
-# Tuple. First value True, then load all compatible hdf5 data  presently saved in 
+# Tuple. First value True, then load all compatible hdf5 data  presently saved in
 # the ~/data directory under today's date. Second value True, then all items
 # are loaded in their 'checked' state.
 auto_load = True, True
@@ -22,10 +22,10 @@ opts = {"foreground": "w"}
 
 
 # Plot Line Colors
-default_colors = [(47,126,243), (250,138,39), (96,233,128), (255,77,77), 
-                  (255,51,153), (128,255,0), (255,241,102), (255,128,128), 
-                  (255,255,192), (255,255,64), (0,255,0), (64, 255,255), 
-                  (0,128, 255), (192,64,192), (255,255,255), (64, 0, 255), 
+default_colors = [(47,126,243), (250,138,39), (96,233,128), (255,77,77),
+                  (255,51,153), (128,255,0), (255,241,102), (255,128,128),
+                  (255,255,192), (255,255,64), (0,255,0), (64, 255,255),
+                  (0,128, 255), (192,64,192), (255,255,255), (64, 0, 255),
                   (128,128,128)]
 
 
@@ -37,18 +37,18 @@ except:
 
 
 # Plotter tabs configuration
-graphConfig = namedtuple("graphConfig", 
+graphConfig = namedtuple("graphConfig",
                   "title name row col rowspan colspan show_points ylims")
 graphConfig.__new__.__defaults__ = ("", "Current", 0, 0, 1, 1, True, [0, 1])
-        
+
 tab_configs = [
     ("Current", [graphConfig(name="Current", ylims=[-2, 2])]),
-    ("Spectrum", [graphConfig()]),
-    ("CalibLines", [graphConfig(name="CalibLine1", title="Line1"), 
+    ("Spectrum", [graphConfig(name="Spectrum")]),
+    ("CalibLines", [graphConfig(name="CalibLine1", title="Line1"),
                     graphConfig(name="CalibLine2", title="Line2", col=1)]),
     ("Rabi", [graphConfig(name="Rabi")]),
     ("Molmer-Sorensen", [graphConfig(name="Molmer-Sorensen")]),
-    ("VAET", [graphConfig(name="vaet_parity", title="VAET Parity", ylims=[-1, 1]), 
+    ("VAET", [graphConfig(name="vaet_parity", title="VAET Parity", ylims=[-1, 1]),
              graphConfig(name="scan_nu_eff", title="Scan Nu_eff", col=1),
               graphConfig(name="vaet_time", title="VAET Time", row=1, colspan=2)]),
     ("Parity", [graphConfig(name="Parity", ylims=[-1, 1])]),
