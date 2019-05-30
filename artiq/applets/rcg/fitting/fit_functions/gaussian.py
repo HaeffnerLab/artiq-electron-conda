95c9c0f28d006ad14a2a65b5097c7f68ef281039
Joseph Broz <jbroz@berkeley.edu>
2019-05-30 14:19:19 -0700

    change initial bounds of ramsey window

--- artiq/applets/rcg/fitting/fit_functions/gaussian.py
+++ artiq/applets/rcg/fitting/fit_functions/gaussian.py
@@ -6,3 +6,10 @@ Tex = r"A\cdot exp[-(x-x_0)^2/2\sigma^2]"
 
 def fit_function(x, A=.5, x0=0, sigma=1):
     return A * np.exp(-(x - x0)**2 / (2 * sigma**2))
+
+def guess_parameters(xdata, ydata):
+    A = np.max(ydata)
+    x0 = xdata[np.argmax(ydata)]
+    std = np.std(ydata)
+    sigma = np.abs(xdata[np.argmin(ydata - std)] - x0)
+    return A, x0, sigma
--- artiq/applets/rcg/fitting/fit_functions/sinesquared.py
+++ artiq/applets/rcg/fitting/fit_functions/sinesquared.py
@@ -2,7 +2,18 @@ import numpy as np
 
 name = "sine^2"
 
-Tex = r"A sin^2(\frac{2\pi}{T}\cdot x + \phi) + B"
+Tex = r"A sin^2(2\pi \cdot freq[kHz] \cdot x + \phi) + B"
 
-def fit_function(x, A=1, T=5, phi=0, B=0):
-    return A * np.sin(2 * np.pi / T * x + phi)**2 + B
+def fit_function(x, A=1, freq=1, phi=0, B=0):
+    return A * np.sin(2 * np.pi * freq * 1e5 * x + phi)**2 + B
+
+def guess_parameters(xdata, ydata):
+    B = np.min(ydata)
+    A = np.max(ydata) - B
+    x = np.array(xdata)
+    y = np.array(ydata)
+    f = np.fft.fftfreq(len(x), (x[1] - x[0]))  # This assumes uniform spacing
+    F = abs(np.fft.fft(y))
+    freq = abs(f[np.argmax(F[1:]) + 1])
+    return A, 0.5 * freq * 1e-5, 0., B
+
