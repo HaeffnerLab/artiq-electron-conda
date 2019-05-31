eb5012247245621549a5be2ad71f145236992884
Joseph Broz <jbroz@berkeley.edu>
2019-05-31 16:46:49 -0700

    small change

--- artiq/dashboard/experiments.py
+++ artiq/dashboard/experiments.py
@@ -433,7 +433,6 @@ class _ExperimentDock(QtWidgets.QMdiSubWindow):
         period = float(self.periodic_schedule_spinbox.value())
         await asyncio.sleep(1e-4)
         expurl = self.expurl
-        expurl.update({"priority": self.priority.value()})
         while True:
             if self.schedule_periodic_bool:
                 self.manager.submit(expurl)
@@ -828,7 +827,7 @@ class ExperimentManager:
         for expurl, dock in self.open_experiments.items():
             self.dock_states[expurl] = dock.save_state()
         for val in self.submission_scheduling.values():
-            val.update({"priority": self.priority.value()})
+            val.update({"priority": dock.priority.value()})
         return {
             "scheduling": self.submission_scheduling,
             "options": self.submission_options,
