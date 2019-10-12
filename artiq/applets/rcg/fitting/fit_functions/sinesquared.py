c242650e2b2adad5f9cfcecc7db419850c3c8331
Joseph Broz <jbroz@berkeley.edu>
2019-10-12 11:41:11 -0700

    PMT MLE and fit function fix

--- artiq/applets/rcg/fitting/fit_functions/sinesquared.py
+++ artiq/applets/rcg/fitting/fit_functions/sinesquared.py
@@ -2,10 +2,10 @@ import numpy as np
 
 name = "sine^2"
 
-Tex = r"A sin^2(2\pi \cdot freq[kHz] \cdot x + \phi) + B"
+Tex = r"A sin^2[(2\pi / 4T_{\pi}) \cdot x + \phi] + B"
 
-def fit_function(x, A=1, freq=1, phi=0, B=0):
-    return A * np.sin(2 * np.pi * freq * 1e5 * x + phi)**2 + B
+def fit_function(x, A=1, tpi=1, phi=0, B=0):
+    return A * np.sin(2 * np.pi * (1/(4*tpi)) * 1e6 * x + phi)**2 + B
 
 def guess_parameters(xdata, ydata):
     B = np.min(ydata)
@@ -15,5 +15,5 @@ def guess_parameters(xdata, ydata):
     f = np.fft.fftfreq(len(x), (x[1] - x[0]))  # This assumes uniform spacing
     F = abs(np.fft.fft(y))
     freq = abs(f[np.argmax(F[1:]) + 1])
-    return A, 0.5 * freq * 1e-5, 0., B
+    return A, 1 / (2.0 * freq * 1e-6), 0., B
 
