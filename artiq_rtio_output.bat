83e09af096baa8c1ea3cc97e69f581a243edac4e
Ryan Shaffer <rmshaffer@gmail.com>
2020-04-15 20:30:44 -0700

    Look for Anaconda3 under user profile and program data folders

--- artiq_rtio_output.bat
+++ artiq_rtio_output.bat
@@ -1,6 +1,7 @@
 @echo off
 title ARTIQ Core Analyzer
-call C:\ProgramData\Anaconda3\Scripts\activate.bat C:\ProgramData\Anaconda3
+if exist "%USERPROFILE%\Anaconda3\Scripts\activate.bat" call "%USERPROFILE%\Anaconda3\Scripts\activate.bat" "%USERPROFILE%\Anaconda3"
+if exist "%PROGRAMDATA%\Anaconda3\Scripts\activate.bat" call "%PROGRAMDATA%\Anaconda3\Scripts\activate.bat" "%PROGRAMDATA%\Anaconda3"
 call activate artiq
 set PYTHONPATH=%PYTHONPATH%;%USERPROFILE%\labrad;%USERPROFILE%\artiq;%USERPROFILE%\artiq\artiq;%USERPROFILE%\artiq\artiq\.pulse_sequence;%USERPROFILE%\artiq-work;%USERPROFILE%\artiq-master
 pushd %USERPROFILE%\artiq-master
