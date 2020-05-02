1bbf1342f329f9100459c9c25d905bac80612fc8
Ryan Shaffer <rmshaffer@gmail.com>
2020-05-02 16:41:03 -0400

    Expand sample Julia code

--- artiq/.pulse_sequence/simulate.jl
+++ artiq/.pulse_sequence/simulate.jl
@@ -6,11 +6,26 @@ function simulate_with_ion_sim(pulses, num_ions)
     # The length of the list returned should be 2**num_ions.
     # i.e., for num_ions=1, the returned list should be of length 2
     #       for num_ions=3, the returned list should be of length 8, etc.
-    
-    # BEGIN TEMP
-    # Call IonSim for real here, using "pulses" and "num_ions".
-    # This is just an example showing how to call some IonSim code:
-    ion = ca40(selected_level_structure=["S-1/2", "D-1/2"])
+
+    ions = []
+    for i = 1:num_ions
+        push!(ions, ca40(selected_level_structure=["S-1/2", "D-1/2"]))
+    end
+
+    for pulse in pulses
+        dds_name = pulse["dds_name"]
+        time_on = pulse["time_on"]
+        time_off = pulse["time_off"]
+        freq = pulse["freq"]
+        amp = pulse["amp"]
+        att = pulse["att"]
+        phase = pulse["phase"]
+
+        # TODO: Do something with this pulse.
+    end
+
+    # TODO: Run the simulation and return the result.
+
     return [0.1, 0.9] # fake single-ion result indicating P(S)=0.1, P(D)=0.9
-    # END TEMP
+
 end
