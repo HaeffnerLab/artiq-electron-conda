cef51ab22215be28db079932a7937ba3501d1798
Ryan Shaffer <rmshaffer@gmail.com>
2020-05-03 09:14:13 -0400

    Add script to start RCG

---
+++ artiq_grapher_start.bat
@@ -0,0 +1,8 @@
+@echo off
+title Real Complicated Grapher
+if exist "%USERPROFILE%\Anaconda3\Scripts\activate.bat" call "%USERPROFILE%\Anaconda3\Scripts\activate.bat" "%USERPROFILE%\Anaconda3"
+if exist "%PROGRAMDATA%\Anaconda3\Scripts\activate.bat" call "%PROGRAMDATA%\Anaconda3\Scripts\activate.bat" "%PROGRAMDATA%\Anaconda3"
+call activate artiq
+set PYTHONPATH=%PYTHONPATH%;%USERPROFILE%\labrad;%USERPROFILE%\artiq;%USERPROFILE%\artiq\artiq;%USERPROFILE%\artiq\artiq\.pulse_sequence;%USERPROFILE%\artiq-work;%USERPROFILE%\artiq-master
+pushd %USERPROFILE%\artiq-work
+python %USERPROFILE%\artiq\artiq\applets\rcg\RealComplicatedGrapher.py & popd
